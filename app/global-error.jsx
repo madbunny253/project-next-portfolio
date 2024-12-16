4c0690036ae8750e72d569b1ce81cb76cc4a4da1
madbunny253 <abodewar253@gmail.com>
2024-12-16 23:50:44 +0530

    first commit

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -1,4 +1,4 @@
-"use client";
+/*"use client";
 
 import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
@@ -16,4 +16,4 @@ export default function GlobalError({ error }) {
       </body>
     </html>
   );
-}
+}*/
